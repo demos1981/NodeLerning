2cf54576c82cfe766d3ed900f13a99dc7635dd33
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-04-01 12:50:29 +0300

    Task4:add middleware

--- app.js
+++ app.js
@@ -54,15 +54,15 @@ mongoService
         res.status(500).json({ error: err.message });
       }
     });
-    // //middleware
-    // app.use((req, res, next) => {
-    //   res.status(404).json({ error: "Not Found" });
-    // });
-    // //middleware
-    // app.use((err, req, res, next) => {
-    //   console.error(err.stack);
-    //   res.status(500).json({ error: "Internal Server Error" });
-    // });
+    //middleware
+    app.use((req, res, next) => {
+      res.status(404).json({ error: "Not Found" });
+    });
+    //middleware
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
