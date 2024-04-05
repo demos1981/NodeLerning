a9abc9345e1286d337b24303930f44e54228af4d
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-04-05 22:25:07 +0300

    Lesson6:change git ignore

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,3 +1,15 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entities/user.entity";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSRGRES_DB,
+  synchronize: true,
+  logging: false,
+  entitites: [User],
+});
