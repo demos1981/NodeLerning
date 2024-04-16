8f0c34b2300c358244d3d22b86423f7b8fbf77b8
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-04-17 01:16:05 +0300

    Lesson9:add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recomended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    tscongigRootDir: __dirname,
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  ignorePatterns: ["**/*.js"],
+
+  rules: {
+    semi: ["error", "always"],
+    indent: ["error", 2],
+    "max-len": ["error", { code: 160 }],
+    quotes: ["error", single, { avoidEscape: true }],
+    eqeqeq: ["error", "smart"],
+    "no-multi-space": "error",
+    "no-multiple-empty-lines": "error",
+    "no-trailing-spaces": "error",
+    "eol-last": ["error", "always"],
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
