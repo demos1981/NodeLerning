e60324dc38ea78c1bd5e53d9d84678bc2ff66d2c
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-04-23 23:57:55 +0300

    Lesson10:change productService.ts

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,12 +1,12 @@
 import express, { Router } from "express";
 import * as productController from "../../src/controllers/productController";
 import authMiddleware from "../../src/middlewares/auth.middleware";
-import { validationMiddleware } from "src/middlewares/validation.middleware";
-import { CreateProductDto } from "src/dto/product.dto";
+import { validationMiddleware } from "../../src/middlewares/validation.middleware";
+import { CreateProductDto } from "../../src/dto/product.dto";
 
 const router: Router = express.Router();
 
-router.get("/", productController.getAllProduct);
+router.get("/", authMiddleware(), productController.getAllProduct);
 router.get(
   "/product-by-id-with-users",
   authMiddleware(),
