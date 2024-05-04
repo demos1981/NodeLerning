9e0456e116f49d82807472da1a906d816428b9d3
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-05-05 02:20:56 +0300

    Lesson12:change userServices.ts

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -2,6 +2,7 @@ import { User } from "../entities/users.entity";
 import bcrypt from "bcrypt";
 import { CreateUserDto } from "src/dto/user.dto";
 import { Product } from "../../src/entities/product.entity";
+import { DataUser } from "src/interfaces/auth.interface";
 
 export const getAllUsers = async () => {
   const [users, count] = await User.findAndCount({});
