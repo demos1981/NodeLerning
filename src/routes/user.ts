c2f949b17bf1fa5df1b364c1465cab290d083940
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-05-05 12:06:53 +0300

    Lesson: delete file middleware

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,15 +3,13 @@ import * as userController from "../controllers/userController";
 import authMiddleware from "../middlewares/auth.middleware";
 import { validationMiddleware } from "../middlewares/validation.middleware";
 import { CreateUserDto } from "../dto/user.dto";
-import { UserRole } from "../../src/interfaces/user.interface";
-import { roleMiddleware } from "../../src/middlewares/acess.middleware";
 
 const router: Router = express.Router();
 
 router.get(
   "/",
   authMiddleware(),
-  roleMiddleware(UserRole.ADMIN, UserRole.OWNER),
+
   userController.getAllUsers
 );
 router.get(
