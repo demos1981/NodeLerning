9eacb3bfed404ca47b96fceb91030d5419228cd3
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-05-10 23:40:59 +0300

    stage: data source change

--- src/data-source.ts
+++ src/data-source.ts
@@ -25,4 +25,5 @@ export const AppDataSource = new DataSource({
   logging: false,
   entities: [User, RefreshToken, Product],
 });
+
 //"postgres://default:Va2puz0nEvTt@ep-broad-water-a4fhh14t.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
