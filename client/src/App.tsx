202d78b22c70d44c08ffddd0d6955658917f4895
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-05-21 16:50:49 +0300

    versus1: change app.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="max-h-screen flex flex-col">
+      <div>Hello</div>
+      <div>World</div>
+    </div>
+  );
 }
