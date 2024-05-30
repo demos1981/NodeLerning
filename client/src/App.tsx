cf3dbe1f60441c77f4ad4c90e2ca7e0ca1cacf00
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-05-30 23:51:26 +0300

    versus1:create store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-
-import Home from "./pages/Home";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
 
--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { authRoutes, publicRoutes } from "./routes";
+import { HOME_ROUTE } from "../utils/consts";
 
 const AppRouter: React.FC = () => {
   const isAuth = false;
@@ -13,6 +14,7 @@ const AppRouter: React.FC = () => {
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} component={Component} exact />
       ))}
+      <Redirect to={HOME_ROUTE} />
     </Switch>
   );
 };
