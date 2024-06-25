6f2f6d5ffdf0ca52456f67d4139a3062d5da85b4
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-06-25 19:56:01 +0300

    versus1: change productPage

--- client/src/components/ProductDisplay.tsx
+++ client/src/components/ProductDisplay.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { ProductProps } from "../types/data";
 
-const ProductDysplay: React.FC<ProductProps> = ({ image, name, color }) => {
+const ProductDysplay: React.FC<ProductProps> = ({
+  id,
+  price,
+  description,
+  image,
+  name,
+  color,
+}) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg p-4">
-      <img className="w-full" src={image} alt={name} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{name}</div>
-        <p className="text-gray-700 text-base">Color: {color}</p>
+    <>
+      <div>{id}</div>
+      <div>{price}</div>
+      <div>{description}</div>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg p-4">
+        <img className="w-full" src={image} alt={name} />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{name}</div>
+          <p className="text-gray-700 text-base">Color: {color}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -4,6 +4,9 @@ import ProductDisplay from "../components/ProductDisplay";
 
 const ProductPage: React.FC = () => {
   const product = {
+    id: 1,
+    price: 100,
+    description: "Product",
     image: "https://via.placeholder.com/150", // Replace with the actual image URL
     name: "Sample Product",
     color: "Red",
@@ -12,6 +15,9 @@ const ProductPage: React.FC = () => {
     <>
       <HeaderLayouts />
       <ProductDisplay
+        id={product.id}
+        price={product.price}
+        description={product.description}
         image={product.image}
         name={product.name}
         color={product.color}
