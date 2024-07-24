f31b237520ece32b5427afa8f5fcfb63aa91215a
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-07-24 18:59:52 +0300

    style:style admin page.tsx

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -7,7 +7,7 @@ const AdminPage: React.FC = () => {
     <>
       <div>
         <h1 className="flex justify-center text-4xl ">Admin page</h1>
-        <h2 className="text-2xl">Welcome to the admin panel</h2>
+
         <AdminProduct />
         <AdminUser />
       </div>
