a92a881023cf7d38f613a93f58ad5c6bf80e39fe
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-07-25 22:42:00 +0300

    style:change style login.tsx

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -4,7 +4,7 @@ import Login from "../components/Login";
 const AuthPage: React.FC = () => {
   return (
     <>
-      <div className=" flex items-center justify-center mt-90">
+      <div className=" flex items-center justify-center mt-90 bg-base-gray text-base-gray-dark">
         <Login />
       </div>
     </>
