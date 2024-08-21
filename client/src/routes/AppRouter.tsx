a2f32db6630c46604d818b46d9e492c6613c0d78
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-08-21 23:34:21 +0300

    routing: add routings

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -3,10 +3,15 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { authRoutes, publicRoutes } from "./routes";
 import {
   ABOUT_ROUTE,
+  ADMIN_ROUTE,
   CONTACTS_ROUTE,
   DELIVERY_ROUTE,
   HOME_ROUTE,
+  LOGIN_ROUTE,
   PAYMENTS_ROUTE,
+  PRODUCT_ADD,
+  PRODUCT_LIST_ROUTE,
+  REGISTRATION_ROUTE,
 } from "../utils/consts";
 import { useAppSelector } from "../hook/hooks";
 import { RootState } from "../store/store";
@@ -16,6 +21,11 @@ import Layouts from "../layouts/Layouts";
 import ContactPage from "../pages/ContactPage";
 import DeliveryPage from "../pages/DeliveryPage";
 import PaymentsPage from "../pages/PaymentsPage";
+import AdminPage from "../pages/AdminPage";
+import AuthPage from "../pages/AuthPage";
+import RegistrPage from "../pages/RegistrPage";
+import ProductListPage from "../pages/ProductListPage";
+import AddProduct from "../components/AddProduct";
 
 const AppRouter: React.FC = () => {
   const isAuthenticated = useAppSelector(
@@ -32,6 +42,12 @@ const AppRouter: React.FC = () => {
         <Route path={DELIVERY_ROUTE} element={<DeliveryPage />} />
         <Route path={PAYMENTS_ROUTE} element={<PaymentsPage />} />
       </Route>
+      <Route path={ADMIN_ROUTE} element={<AdminPage />} />
+      <Route path={LOGIN_ROUTE} element={<AuthPage />} />
+      <Route path={REGISTRATION_ROUTE} element={<RegistrPage />} />
+      <Route path={LOGIN_ROUTE} element={<AuthPage />} />
+      <Route path={PRODUCT_LIST_ROUTE} element={<ProductListPage />} />
+      <Route path={PRODUCT_ADD} element={<AddProduct />} />
     </Routes>
     // </Router>
   );
