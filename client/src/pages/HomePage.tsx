d1e03fbfc64d7f0cb2897ba1b2afb5179327d1f7
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-11-04 19:30:37 +0200

    style: delete routes

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import HeaderLayouts from "../layouts/HeaderLayouts";
-import FooterLayouts from "../layouts/FooterLayouts";
-import { Outlet } from "react-router-dom";
+// import HeaderLayouts from "../layouts/HeaderLayouts";
+// import FooterLayouts from "../layouts/FooterLayouts";
+// import { Outlet } from "react-router-dom";
 import MainLayouts from "../layouts/MainLayouts";
 
 const HomePage: React.FC = () => {
