ff24b10327cc0534119ceff3b32f9a3e4caaf68e
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2025-03-09 00:45:47 +0200

    uiChange: change login component

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useAppDispatch, useAppSelector } from "hooks/hooks";
 import { RootState } from "app/store/store";
 import { login } from "features/auth/authSlice";
+import { Link } from "react-router-dom";
 
 export const Login: React.FC = () => {
   const [email, setEmail] = useState("");
@@ -15,8 +16,11 @@ export const Login: React.FC = () => {
   };
 
   return (
-    <div className=" min-h-screen flex items-center justify-center max-w-md bg-base-gray text-base-gray-light ">
-      <div className=" p-8 rounded-md shadow-md  max-w-md bg-base-gray-dark text-base-gray-light">
+    <div className=" min-h-screen flex items-center   text-base-gray-light ">
+      <div className="max-w-3xl">
+        <img src="/assets/Login.jpg" alt="login"></img>
+      </div>
+      <div className=" p-8 rounded-md   w-2/6  text-base-gray-dark">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
         <form onSubmit={handleLogin}>
           <div className="mb-4">
@@ -38,6 +42,15 @@ export const Login: React.FC = () => {
               className="mt-2 px-4 py-2 border rounded w-full text-base-gray-dark"
               required
             />
+          </div>
+          <div className="mb-10 flex justify-between">
+            <div>
+              <input type="checkbox" id="remember" name="scales" />
+              <label htmlFor="remember" className="ml-3 ">
+                Remember Me
+              </label>
+            </div>
+            <span>Forgot Password?</span>
           </div>
           <button
             type="submit"
